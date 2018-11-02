69ff00fa194f39857c47ff7a76d2d409a463df07
YoannFoulon <yoann.foulon@etu.univ-amu.fr>
2018-11-02 18:05:13 +0100

    Début tests unitaires: Page d'accueil et register

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -6,13 +6,45 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testApp()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/app');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertCount(5, $crawler->filter('div'));
+
+        $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'test',
+            'PHP_AUTH_PW'   => 'test',
+        ));
+
+        $crawler = $client->request('GET', '/app');
+        $this->assertCount(7, $crawler->filter('div'));
+    }
+
+    public function testAppCoordinates()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/app', array(
+            'lat' => 17,
+            'lon' => 45
+        ));
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertCount(5, $crawler->filter('div'));
+
+        $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'test',
+            'PHP_AUTH_PW'   => 'test',
+        ));
+
+        $crawler = $client->request('GET', '/app', array(
+            'lat' => 17,
+            'lon' => 45
+        ));
+        $this->assertCount(7, $crawler->filter('div'));
     }
 }
